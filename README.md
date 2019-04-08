ddea58738d2d2996c307bb44be6975b1387b4cc8
JGadi <johnpgadi@gmail.com>
2019-04-08 12:11:06 -0700

    completed change calculator

---
+++ README.md
@@ -0,0 +1,5 @@
+Hello, welcome to the Change Calculator. 
+
+This will calculate the change in dollars, quarters, dimes, nickels, and pennies, from the amount due and amount received.
+
+Load the Change Calculator by typing "npm start" in the command line and going to http://127.0.0.1:8080/ in your browser. Thanks for visiting!
--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 // Write your JavaScript here
+document.getElementById("calculate-change").onclick = function() {
+ 
+      var received = document.getElementById("amount-received").value;
+      var due = document.getElementById("amount-due").value;
+      var total = received - due;
+      var change = total.toFixed(2);
+
+      var dollars = 0;
+      var quarters = 0;
+      var dimes = 0;
+      var nickels = 0;
+      var pennies = 0;
+  
+     dollars = Math.floor(change/1);
+     change = (change % 1);
+      document.getElementById("dollars-output").innerText = dollars;
+
+     quarters = Math.floor(change/.25);
+     change = (change % .25);
+      document.getElementById("quarters-output").innerText = quarters;
+
+     dimes = Math.floor(change/.10);
+     change = (change % .10);
+      document.getElementById("dimes-output").innerText = dimes;
+     
+     nickels = Math.floor(change/.05);
+     change = (change % .05);
+      document.getElementById("nickels-output").innerText = nickels;
+      
+     pennies = Math.floor(change/.01);
+     change = (change % .01);
+      document.getElementById("pennies-output").innerText = pennies; 
+    };
